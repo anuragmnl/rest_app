9ae1b78b64ba3fbd9762b2e16b706449824ead60
anurag mishra <anurag171@gmail.com>
2022-05-01 23:33:43 +0530

    Handling Exceptions and WebUri

---
+++ src/main/java/com/smaato/task/constant/TaskConstant.java
@@ -0,0 +1,14 @@
+package com.smaato.task.constant;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class TaskConstant {
+
+  public static final String FAILED = "failed";
+  public static final String OK = "ok";
+  public static final String TIME_KEY = "HH:mm";
+
+
+}
